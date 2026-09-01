398098b21ce4c301cb456aee49fa7b372e79dd8c
Vukasin Radovanovic <vukasinradovanovic@gmail.com>
2026-09-01 22:19:26 +0200

    Activation for account added

---
+++ FlowDeskAPI/Controllers/Auth/ActivateAccountController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Flowdesk.Commands.Auth;
+using Implementation.Permissions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowDeskAPI.Controllers.Auth
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ActivateAccountController : ControllerBase
+    {
+        [HttpGet("{activationCode}")]
+        public ActionResult Activate([FromServices] IActivateAccountCommand command,
+                                     [FromServices] PermissionHandler handler,
+                                     string activationCode)
+        {
+            handler.ExecuteCommand(command, activationCode);
+            return Redirect("http://localhost:5173/login?activated=true");
+        }
+    }
+}
